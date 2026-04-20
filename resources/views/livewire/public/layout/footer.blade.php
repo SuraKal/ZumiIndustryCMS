e2d3fde9245c96ef06aeec7af8a32b28f4f41e40
Surafel Kaleab <surafelkaleab68@gmail.com>
2026-04-20 12:41:01 -0700

    added some public pages

--- resources/views/livewire/public/layout/footer.blade.php
+++ resources/views/livewire/public/layout/footer.blade.php
@@ -68,213 +68,27 @@ new class extends Component {
 ?>
 
 
-<div>
-    <livewire:public.sections.socialmedia />
-        <!--  About Img Section -->
-    <section class="about-img">
-        <div class="marquee w-100 d-flex align-items-center overflow-hidden bg-primary py-4">
-            <div class="marquee-content d-flex align-items-center gap-8">
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Electrical Installation</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">CCTV & Security</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Generator Services</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Fire Alarms</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Maintenance</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Communication Systems</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Electro-Mechanical Works</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Electrical Installation</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">CCTV & Security</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Generator Services</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Fire Alarms</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Maintenance</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Communication Systems</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Electro-Mechanical Works</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Electrical Installation</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">CCTV & Security</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Generator Services</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Fire Alarms</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Maintenance</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Communication Systems</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-                <div class="hstack gap-4 flex-shrink-0">
-                    <h4 class="mb-0">Electro-Mechanical Works</h4>
-                    <span class="round-10 bg-dark bg-opacity-10 rounded-circle flex-shrink-0"></span>
-                </div>
-            </div>
-        </div>
-    </section>
-    <livewire:public.sections.contact_1 />
-
-
-
-
-    <footer class="footer bg-dark py-5 py-lg-10">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-5 mb-8 mb-xl-0">
-                    <div class="d-flex flex-column gap-8 pe-xl-5">
-                        <h2 class="mb-0 text-white">Ready to power your project?</h2>
-                        <div class="d-flex flex-column gap-2">
-                            
-                            
-                            @if(!empty($emails))
-
-                            <span class="hstack gap-3 text-white fs-5">
-                                <iconify-icon icon="lucide:arrow-up-right" class="fs-7 text-primary"></iconify-icon>
-                                
-
-                                        @foreach($emails as $index => $email)
-                                            <a href="mailto:{{ $email }}" class="text-white fs-5 ">
-                                                {{ $email }}
-                                            </a>
-                                        @if($index < count($emails) - 1) / @endif @endforeach 
-                            </span>
-                            @endif
-
-
-                            @if(!empty($phones))
-
-                            <span class="hstack gap-3 text-white fs-5">
-                                <iconify-icon icon="lucide:phone" class="fs-7 text-primary"></iconify-icon>
-                                
-
-                                        @foreach($phones as $index => $phone)
-                                            <a href="tel:{{ $phone }}" class="text-white fs-5 ">
-                                                {{ $phone }}
-                                            </a>
-                                        @if($index < count($phones) - 1) / @endif @endforeach 
-                            </span>
-                            @endif
-                            
-                            @if(!empty($address))
-                            <a
-                                class="link-hover hstack gap-3 text-white fs-5">
-                                <iconify-icon icon="lucide:map-pin" class="fs-7 text-primary"></iconify-icon>
-                                {{ $address }}
-                            </a>
-                            @endif
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 col-xl-2 mb-8 mb-xl-0">
-                    <ul class="footer-menu list-unstyled mb-0 d-flex flex-column gap-2">
-                        <li>
-                            <a class="link-hover fs-5 text-white" href="{{ route('home') }}">Home</a>
-                        </li>
-                        <li>
-                            <a class="link-hover fs-5 text-white" href="{{ route('public.about') }}">About</a>
-                        </li>
-                        <li>
-                            <a class="link-hover fs-5 text-white" id="services" href="{{ route('public.services') }}">Services</a>
-                        </li>
-                        <li>
-                            <a class="link-hover fs-5 text-white" href="{{ route('public.projects') }}">Projects</a>
-                        </li>
-                        <li>
-                            <a class="link-hover fs-5 text-white" href="{{ route('public.galleries') }}">Gallery</a>
-                        </li>
-                        <li>
-                            <a class="link-hover fs-5 text-white" href="{{ route('public.contact') }}">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                
-                <livewire:public.sections.footer_socialmedia />
-
-                <div class="col-md-3 col-xl-3 mb-8 mb-xl-0">
-                    <h5 class="text-white mb-4">Quick Links</h5>
-                    <ul class="footer-menu list-unstyled mb-0 d-flex flex-column gap-2">
-                        <li>
-                            <a class="link-hover fs-6 text-white text-opacity-75"
-                                href="{{ route('public.about') }}">About
-                                Us</a>
-                        </li>
-                        <li>
-                            <a class="link-hover fs-6 text-white text-opacity-75" href="{{ route('public.services') }}">Our Services</a>
-                        </li>
-                        <li>
-                            <a class="link-hover fs-6 text-white text-opacity-75" href="{{ route('home') }}#why-us">Why
-                                Choose
-                                Us</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row mt-4">
-                <div class="col-12">
-                    <p class="mb-0 text-white text-opacity-70 text-center">
-                        © The Amen Trading PLC copyright {{ date('Y') }}. All rights reserved. <br>
-                        Maintained By
-                        <a href="https://www.instagram.com/naeldesigns/" target="_blank" class="text-primary">Nael Production</a>.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-
-    <div class="get-template hstack gap-2">
-        <button class="btn bg-primary p-2 round-52 rounded-circle hstack justify-content-center flex-shrink-0"
-            id="scrollToTopBtn">
-            <iconify-icon icon="lucide:arrow-up" class="fs-7 text-dark"></iconify-icon>
-        </button>
+<footer class="py-5 text-center border-top border-dark">
+    <div class="container">
+    <a class="navbar-brand logo" href="#hero">
+        <img
+        src="./assets/img/new/logo.png"
+        height="80"
+        alt="ZUMI INDUSTRY PLC Logo"
+        />
+        <!-- style="filter: brightness(0) invert(1)" -->
+    </a>
+    <p class="text-gray small mb-0 mt-2">
+        © 2025 ZUMI INDUSTRY PLC. The First & Only Dry Cell Factory in
+        Ethiopia.
+    </p>
+    <p>
+        Maintained by
+        <a
+        href="https://www.instagram.com/naeldesigns/"
+        class="text-white text-decoration-none"
+        >Nael Production</a
+        >
+    </p>
     </div>
-</div>
+</footer>
--- resources/views/livewire/public/layout/header.blade.php
+++ resources/views/livewire/public/layout/header.blade.php
@@ -8,76 +8,42 @@ new class extends Component {
 };?>
 
 
-<header class="header border-4 border-primary border-top position-fixed start-0 top-0" style="width:100vw !important;">
+<nav class="navbar navbar-expand-lg fixed-top py-3">
     <div class="container">
-        <div class="header-wrapper d-flex align-items-center justify-content-between">
-            <div class="logo">
-                <a href="{{ route('home') }}" class="logo-white">
-                    <img src="{{ asset('static/assets/images/logos/logo2.png') }}" alt="logo" class="img-fluid" style="height: 85px" />
-                </a>
-                <a href="{{ route('home') }}" class="logo-dark">
-                    <img src="{{ asset('static/assets/images/logos/logo4.png') }}" alt="logo" class="img-fluid" style="height: 60px" />
-                </a>
-            </div>
-            <div class="d-flex align-items-center gap-4">
-                <div class="btn-group">
-                    <button
-                        class="btn btn-secondary toggle-menu round-45 p-2 d-flex align-items-center justify-content-center bg-white rounded-circle mr-2"
-                        type="button" data-bs-toggle="dropdown" data-bs-auto-close="true" aria-expanded="false">
-                        <iconify-icon icon="solar:hamburger-menu-line-duotone" class="menu-icon fs-8 text-dark">
-                        </iconify-icon>
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-end p-4">
-                        <div class="d-flex flex-column gap-6">
-                            <div class="hstack justify-content-between border-bottom pb-6">
-                                <p class="mb-0 fs-5 text-dark">Menu</p>
-                                <button type="button" class="btn-close opacity-75" aria-label="Close"></button>
-                            </div>
-                            <div class="d-flex flex-column gap-3">
-                                <ul class="header-menu list-unstyled mb-0 d-flex flex-column gap-2">
-
-                                    <x:public.links.nav-link route="home">Home</x:public.links.nav-link>
-                                    <x:public.links.nav-link route="public.about">About</x:public.links.nav-link>
-                                    <x:public.links.nav-link route="public.services">Services</x:public.links.nav-link>
-                                    <x:public.links.nav-link route="public.products">Products</x:public.links.nav-link>
-                                    <x:public.links.nav-link route="public.galleries">Gallery</x:public.links.nav-link>
-                                    <x:public.links.nav-link route="public.projects">Projects</x:public.links.nav-link>
-                                    <x:public.links.nav-link route="public.blogs">Blog</x:public.links.nav-link>
-                                    <x:public.links.nav-link route="public.contact">Contact</x:public.links.nav-link>
-
-                                </ul>
-                                {{-- <div class="hstack">
-                                    <div class="d-flex align-items-center gap-3 mx-2">
-                                        <a href="https://instagram.com/TATPLC16" target="_blank"
-                                            rel="noopener noreferrer" class="text-black" aria-label="Instagram">
-                                            <iconify-icon icon="fa-brands:instagram" class="fs-5"></iconify-icon>
-                                        </a>
-                                        <a href="https://facebook.com/TATPLC16" target="_blank"
-                                            rel="noopener noreferrer" class="text-black" aria-label="Facebook">
-                                            <iconify-icon icon="fa-brands:facebook" class="fs-5"></iconify-icon>
-                                        </a>
-                                        <a href="https://twitter.com/TATPLC16" target="_blank" rel="noopener noreferrer"
-                                            class="text-black" aria-label="Twitter">
-                                            <iconify-icon icon="fa-brands:twitter" class="fs-5"></iconify-icon>
-                                        </a>
-                                        <a href="https://www.linkedin.com/company/the-amen-trading-plc" target="_blank"
-                                            rel="noopener noreferrer" class="text-black" aria-label="LinkedIn">
-                                            <iconify-icon icon="fa-brands:linkedin" class="fs-5"></iconify-icon>
-                                        </a>
-                                    </div>
-                                </div> --}}
-
-                                <livewire:public.sections.header_socialmedia>
-                            </div>
-                            <div>
-                                <a class="text-dark" href="tel:+251901368836">+251 901 368 836</a>
-                                <a class="text-dark"
-                                    href="mailto:theamentradingplc16@gmail.com">theamentradingplc16@gmail.com</a>
-                            </div>
-                        </div>
-                    </ul>
-                </div>
-            </div>
+        <a class="navbar-brand logo" href="#hero">
+            <img src="./assets/img/new/logo.png" height="80" alt="ZUMI INDUSTRY PLC Logo" />
+            <!-- style="filter: brightness(0) invert(1)" -->
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" style="
+              background-image: url(&quot;data:image/svg+xml,%3csvg xmlns=%27http://www.w3.org/2000/svg%27 viewBox=%270 0 30 30%27%3e%3cpath stroke=%27rgba%28255, 255, 255, 0.75%29%27 stroke-linecap=%27round%27 stroke-miterlimit=%2710%27 stroke-width=%272%27 d=%27M4 7h22M4 15h22M4 23h22%27/%3e%3c/svg%3e&quot;);
+            "></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="products.html">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="contact.html" class="accent-btn" style="
+                    padding: 10px 25px;
+                    font-size: 0.9rem;
+                    text-decoration: none;
+                ">Get in Touch</a>
+                </li>
+            </ul>
         </div>
     </div>
-</header>
+</nav>
